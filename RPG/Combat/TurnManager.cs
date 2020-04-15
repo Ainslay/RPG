96301d2ab2ad9870b1f633bd2b92b5df84791c37
Jakub Spałek <spalek9822@gmail.com>
2020-04-15 19:05:54 +0200

    Further improved fighters sorting mechanism

--- RPG/Combat/TurnManager.cs
+++ RPG/Combat/TurnManager.cs
@@ -32,25 +32,28 @@ namespace RPG.Combat
         {
             public int Compare(BaseCharacter fighter1, BaseCharacter fighter2)
             {
-                if(fighter1.GetCurrentIniciative() < fighter2.GetCurrentIniciative())
+                if(GreaterThan(fighter1, fighter2))
                 {
                     return 1;
                 }
-                else if(fighter1.GetCurrentIniciative() > fighter2.GetCurrentIniciative())
+                else if(LowerThan(fighter1, fighter2))
                 {
                     return -1;
                 }
                 else
                 {
-                    var random = new Random().Next(0,100);
-
-                    if(random >= 50)
-                    {
-                        return 1;
-                    }
-
-                    return 0;
+                    return new Random().Next(0, 1);
                 }
+            }
+
+            private bool GreaterThan(BaseCharacter fighter1, BaseCharacter fighter2)
+            {
+                return fighter1.GetCurrentIniciative() > fighter2.GetCurrentIniciative();
+            }
+
+            private bool LowerThan(BaseCharacter fighter1, BaseCharacter fighter2)
+            {
+                return fighter1.GetCurrentIniciative() < fighter2.GetCurrentIniciative();
             }
         }
     }
