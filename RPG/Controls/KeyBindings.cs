efa1750714c4b9eecda012afb5fe1e809fe4e7d6
Jakub Spałek <spalek9822@gmail.com>
2020-05-01 13:54:29 +0200

    Character creator input now supports custom keybindings

---
+++ RPG/Controls/KeyBindings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RPG.Controls
+{
+    enum KeyBindings
+    {
+        Key1 = ConsoleKey.D1,
+        Key2 = ConsoleKey.D2,
+        Key3 = ConsoleKey.D3,
+        Key4 = ConsoleKey.D4,
+        Key5 = ConsoleKey.D5,
+        Key6 = ConsoleKey.D6,
+        Key7 = ConsoleKey.D7,
+        Key8 = ConsoleKey.D8,
+        Key9 = ConsoleKey.D9,
+        ESC  = ConsoleKey.Escape,
+        ENTER = ConsoleKey.Enter
+    }
+}
