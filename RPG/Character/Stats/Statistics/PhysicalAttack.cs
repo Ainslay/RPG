4cd7d9549485099eb972204014fd14478e9adbaa
Jakub Spałek <spalek9822@gmail.com>
2020-05-13 14:15:41 +0200

    Added PhyscialAttack class which inherits BaseStatistic

---
+++ RPG/Character/Stats/Statistics/PhysicalAttack.cs
@@ -0,0 +1,9 @@
+﻿namespace RPG.Character.Stats
+{
+    class PhysicalAttack : BaseStatistic
+    {
+        public PhysicalAttack(Strength strength) 
+            : base(strength)
+        { }
+    }
+}
