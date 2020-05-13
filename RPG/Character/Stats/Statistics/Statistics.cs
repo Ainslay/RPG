cc898c0b99c8e7bb640e5cfc47435deb5aef2230
Jakub Spałek <spalek9822@gmail.com>
2020-05-13 14:18:19 +0200

    Updated Statistics to now use PhysicalAttack and MagicAttack instead of AttackStrength

--- RPG/Character/Stats/Statistics/Statistics.cs
+++ RPG/Character/Stats/Statistics/Statistics.cs
@@ -1,16 +1,20 @@
-﻿namespace RPG.Character.Stats
+﻿using System.Collections.Generic;
+
+namespace RPG.Character.Stats
 {
     class Statistics : IRestorable
     {
+        private PhysicalAttack _physicalAttack;
+        private MagicAttack _magicAttack;
         private Resistances _resistances;
-        private AttackStrength _attackStrength;
         private Iniciative _iniciative;
         private HitChance _hitChance;
 
         public Statistics(Attributes attributes)
         {
+            _physicalAttack = new PhysicalAttack(attributes.GetStrength());
+            _magicAttack = new MagicAttack(attributes.GetIntelligence());
             _resistances = new Resistances(attributes.GetIntelligence());
-            _attackStrength = new AttackStrength(attributes.GetStrength(), attributes.GetIntelligence());
             _iniciative = new Iniciative(attributes.GetDexterity());
             _hitChance = new HitChance(attributes.GetDexterity());
         }
@@ -26,12 +30,12 @@
         }
         public int GetPhysicalAttack()
         {
-            return _attackStrength.GetPhysicalAttack();
+            return _physicalAttack.GetCurrentValue();
         }
 
         public int GetMagicAttack()
         {
-            return _attackStrength.GetMagicAttack();
+            return _magicAttack.GetCurrentValue();
         }
 
         public int GetCurrentIniciative()
@@ -46,11 +50,12 @@
 
         public void RestoreBaseValue()
         {
+            var restorableStats = new List<IRestorable> { _physicalAttack, _magicAttack, _hitChance, _resistances, _iniciative };
+
+            restorableStats.ForEach(stat => stat.RestoreBaseValue());
             _resistances.RestoreBaseValue();
             _iniciative.RestoreBaseValue();
             _hitChance.RestoreBaseValue();
-
-            // TODO: restore base attack strength
         }
     }
 }
