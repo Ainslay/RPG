2bf4a36724c2f1cee0f5ca2b8a7b7259e9341b29
Jakub Spałek <spalek9822@gmail.com>
2020-05-15 13:57:06 +0200

    Added abstract class Item from which each piece of equipment will derive

---
+++ RPG/Items/Item.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using RPG.Utilities;
+
+namespace RPG.Items
+{
+    abstract class Item
+    {
+        private string _name;
+        private string _flavorText;
+        private int _value;
+        private int _weight;
+
+        private int _bonusStrength;
+        private int _bonusDexterity;
+        private int _bonusIntelligence;
+
+        public Item(string name, string flavorText, int bonusStrength, int bonusDexterity, int bonusIntelligence, int value, int weight)
+        {
+            ParamCheck.IsNullOrWhitespace(name);
+            ParamCheck.IsNullOrWhitespace(flavorText);
+            ParamCheck.IsBelowZero(bonusStrength);
+            ParamCheck.IsBelowZero(bonusDexterity);
+            ParamCheck.IsBelowZero(bonusIntelligence);
+            ParamCheck.IsBelowZero(value);
+            ParamCheck.IsBelowZero(weight);
+
+            _name = name;
+            _flavorText = flavorText;
+            _bonusStrength = bonusStrength;
+            _bonusDexterity = bonusDexterity;
+            _bonusIntelligence = bonusIntelligence;
+            _value = value;
+            _weight = weight;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public string GetFlavorText()
+        {
+            return _flavorText;
+        }
+
+        public int GetBonusStrength()
+        {
+            return _bonusStrength;
+        }
+
+        public int GetBonusDexterity()
+        {
+            return _bonusDexterity;
+        }
+
+        public int GetBonusIntelligence()
+        {
+            return _bonusIntelligence;
+        }
+
+        public int GetValue()
+        {
+            return _value;
+        }
+
+        public int GetWeight()
+        {
+            return _weight;
+        }
+
+        public void PrintDetails()
+        {
+            Console.WriteLine(_name);
+            Console.WriteLine(_flavorText);
+            Console.WriteLine($"Value: {_value}\tWeight: {_weight}");
+        }
+    }
+}
