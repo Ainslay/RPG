64bbd97fd46124211dcb62b3b4176c6d48334afe
Jakub Spałek <spalek9822@gmail.com>
2020-05-21 13:34:29 +0200

    You can now add items to database via add_item request

---
+++ RPG.API/Controllers/ItemController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using RPG.API.Database;
+using RPG.API.Model;
+
+namespace RPG.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ItemController : ControllerBase
+    {
+        private ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        [Route("add_item")]
+        public IActionResult AddItem(Item item)
+        {
+            _context.Items.Add(item);
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
