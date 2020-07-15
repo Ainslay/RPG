ed5671b75021ec7c43c9193dc6bc838b322679e3
Jakub Spałek <spalek9822@gmail.com>
2020-07-15 16:17:31 +0200

    Added PlayerItem and its configuration

---
+++ RPG.API/Database/Configurations/PlayerItemConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RPG.API.Model;
+
+namespace RPG.API.Database.Configurations
+{
+    public class PlayerItemConfiguration : IEntityTypeConfiguration<PlayerItem>
+    {
+        public void Configure(EntityTypeBuilder<PlayerItem> builder)
+        {
+            builder.ToTable("PlayerItems");
+            builder.HasKey(i => i.PlayerItemId);
+            builder.Property(i => i.PlayerId).IsRequired();
+            builder.Property(i => i.ItemId).IsRequired();
+            builder.Property(i => i.IsEquiped).IsRequired();
+        }
+    }
+}
---
+++ RPG.API/Model/PlayerItem.cs
@@ -0,0 +1,10 @@
+﻿namespace RPG.API.Model
+{
+    public class PlayerItem
+    {
+        public int PlayerItemId { get; set; }
+        public int PlayerId { get; set; }
+        public int ItemId { get; set; }
+        public bool IsEquiped { get; set; }
+    }
+}
