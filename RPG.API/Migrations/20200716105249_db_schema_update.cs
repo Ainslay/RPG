71ef5127d49f4921f00ee32672ca45c9ec617ce0
Jakub Spałek <spalek9822@gmail.com>
2020-07-16 12:56:56 +0200

    Added updated migration

---
+++ RPG.API/Migrations/20200716105249_db_schema_update.cs
@@ -0,0 +1,318 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RPG.API.Migrations
+{
+    public partial class db_schema_update : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Items_Players_PlayerId",
+                table: "Items");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Players",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Items_PlayerId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Health",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Resource",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "BonusDexterity",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "BonusIntelligence",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "BonusStrength",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Value",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Proffesion",
+                table: "Players",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Experience",
+                table: "Players",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlavorText",
+                table: "Items",
+                maxLength: 500,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(300)",
+                oldMaxLength: 300,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ItemId",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Dexterity",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Intelligence",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Strength",
+                table: "Items",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Players",
+                table: "Players",
+                column: "PlayerId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "ItemId");
+
+            migrationBuilder.CreateTable(
+                name: "PlayerItem",
+                columns: table => new
+                {
+                    PlayerItemId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PlayerId = table.Column<int>(nullable: false),
+                    ItemId = table.Column<int>(nullable: false),
+                    IsEquiped = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlayerItem", x => x.PlayerItemId);
+                    table.ForeignKey(
+                        name: "FK_PlayerItem_Items_ItemId",
+                        column: x => x.ItemId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PlayerItem_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "PlayerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerItem_ItemId",
+                table: "PlayerItem",
+                column: "ItemId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PlayerItem_PlayerId",
+                table: "PlayerItem",
+                column: "PlayerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlayerItem");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Players",
+                table: "Players");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Items",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Experience",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "ItemId",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Dexterity",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Intelligence",
+                table: "Items");
+
+            migrationBuilder.DropColumn(
+                name: "Strength",
+                table: "Items");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Proffesion",
+                table: "Players",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Health",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Resource",
+                table: "Players",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FlavorText",
+                table: "Items",
+                type: "nvarchar(300)",
+                maxLength: 300,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 500,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Id",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BonusDexterity",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BonusIntelligence",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "BonusStrength",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId",
+                table: "Items",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Value",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Weight",
+                table: "Items",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Players",
+                table: "Players",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Items",
+                table: "Items",
+                column: "Id");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_PlayerId",
+                table: "Items",
+                column: "PlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Items_Players_PlayerId",
+                table: "Items",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
